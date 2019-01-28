739140a7d7e27c9c656030c77293852f12c23a60
ZhenTrinh <zhenhui.trinh@milliman.com>
2019-01-28 12:14:03 -0600

    Check if required packages exist. If yes, load in the package, otherwise, install, then load in

--- global.R
+++ global.R
@@ -1,10 +1,17 @@
-library(shiny)
-library(dplyr)
-library(plotly)
-library(billboarder)
-library(mlbench)
-library(shinydashboard)
-library(DT)
+# Function to check whether package is installed
+is.installed <- function(pkg){
+  is.element(pkg, installed.packages()[,1])
+} 
+
+# check if package "pacman" is installed
+if (!is.installed("pacman")){
+  install.packages("pacman")
+} else {
+  library(pacman)
+}
+
+p_load(shiny, shinydashboard, dplyr, plotly, billboarder, mlbench)
+
 data("PimaIndiansDiabetes")
 
 
