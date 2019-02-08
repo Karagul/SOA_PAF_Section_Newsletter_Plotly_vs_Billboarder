3409624b27acb9099d727b2174c0a43262721550
Ztrinh <39279238+Ztrinh@users.noreply.github.com>
2019-02-08 09:38:26 -0800

    Update LoadLibraries.R

--- LoadLibraries.R
+++ LoadLibraries.R
@@ -5,7 +5,3 @@ library(billboarder)
 library(mlbench)
 library(shinydashboard)
 library(DT)
-data("PimaIndiansDiabetes")
-
-Bright.Blue <- rgb(red = 0, green = 129, blue = 227, maxColorValue = 255)
-Gold <- rgb(red = 255, green = 162, blue = 0, maxColorValue = 255)
